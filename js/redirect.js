696b5565408afb05967f14aefd88392110a10e52
Hira703 <soniahira48@gmail.com>
2025-03-02 10:59:46 +0600

    added ridirect.js

--- js/redirect.js
+++ js/redirect.js
@@ -1,7 +1,7 @@
 
-document.getElementById("redirect-button").addEventListener("click",function(){
-window.location.href="Blog.html";
-});
-document.getElementById("back-button").addEventListener("click",function(){
-    window.location.href="index.html";
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("redirect-button").addEventListener("click", function () {
+        window.location.href = "Blog.html";
     });
+   
+});
